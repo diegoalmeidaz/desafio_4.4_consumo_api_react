3112a3eeb679cda03917932a3e94c8f2a740c740
Diego Almeida <diegoalmeidaz@gmail.com>
2022-09-14 01:16:26 -0300

    fixes pre tailwind install

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,13 +1,18 @@
 import Form from "react-bootstrap/Form";
 
+
+
 const Search = ({ setSearch }) => {
   const searchValue = (search) => {
     setSearch(search);
   };
 
   return (
-    <div className="p-10 mb-2 mt-5 mb-5">
-        <h1>Filtra por Personaje, Especie, Ubicación o si Vive o No!</h1>
+    <div className="p-10 mb-2 mt-5 mb-5 justify-content-center">
+        <h1 className="justify-content-center">Filtra por Personaje, Especie, Ubicación o si Vive o No!</h1>
+
+    
+
     <Form.Control
       size="text"
       className="mt-4"
